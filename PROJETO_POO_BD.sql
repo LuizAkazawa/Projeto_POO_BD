7ceb405a50625c2cb516a371d810cb0717da39e4
Akazawa <89152151+LuizAkazawa@users.noreply.github.com>
2023-11-23 10:14:42 -0300

    Update PROJETO_POO_BD.sql

--- PROJETO_POO_BD.sql
+++ PROJETO_POO_BD.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS Acoes(
 CREATE TABLE IF NOT EXISTS Conta_has_Acoes (
 	Conta_codConta INT NOT NULL,
     Acoes_siglaAcoes VARCHAR(6),
+    quantidade INT,
     
     PRIMARY KEY (Conta_codConta, Acoes_siglaAcoes),
     
@@ -63,29 +64,25 @@ CREATE TABLE IF NOT EXISTS Conta_has_Acoes (
     
 );
 
-CREATE TABLE IF NOT EXISTS Fundo_Imobiliario(
-	sigla VARCHAR(6) NOT NULL,
-    cotacao DOUBLE NOT NULL,
-    tipo VARCHAR(45) NULL,
-    
-    PRIMARY KEY (sigla)
-);
 
-CREATE TABLE IF NOT EXISTS Conta_has_FIIs (
-	Conta_codConta INT NOT NULL,
-    FIIs_siglaFIIs VARCHAR(6) NOT NULL,
-    
-    PRIMARY KEY (Conta_codConta, FIIs_siglaFIIs),
-    
-	CONSTRAINT fk_Conta_has_FIIs_Conta
-    FOREIGN KEY (Conta_codConta) REFERENCES Conta (cod_conta),
-    
-	CONSTRAINT fk_Conta_has_FIIs_FIIs
-    FOREIGN KEY (FIIs_siglaFIIs) REFERENCES Fundo_Imobiliario(sigla)  
-    
-);
+INSERT INTO Homebroker (url, ip) values ("www.naosei.com", "1");
+
+INSERT INTO Usuario (nome,email,cpf,data_nascimento,Homebroker_ipHomebroker) values 
+("Luiz Akazawa", "luiz@gmail.com","123", "2023-11-20", "1"),
+("Maria Seilá", "maria@gmail.com", "3123","2023-11-20", "1");
+
+INSERT INTO Conta (username,senha,tipo_conta,saldo,data_criacao,Usuario_cpfUsuario) values 
+("Luiz", "1", "Black", 12345.20, "2023-11-12", "123"),
+("Marie", "54321", "Gold", 145.20, "2023-11-30", "3123");
+
+INSERT INTO Acoes (sigla,cotacao,empresa_proprietaria) values
+("FLRY3", 20.34, "Fleury"),
+("MGLU3", 2.22, "Magazine Luiza"),
+("BBAS3", 50.66, "Banco do Brasil")
+;
 
 select * from conta;
 select * from usuario;
 select * from homebroker;
-select * from usuario where cpf = '123';
+select * from acoes;
+select * from conta_has_acoes;
